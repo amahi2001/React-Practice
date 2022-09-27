69efafd7a918e175e0ea05bea5b3d5f3c859abf0
Abrar <amahi2001@gmail.com>
2022-09-27 16:11:13 -0400

    passing data between components with props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      title: "yeee Insurance",
+      amount: 297,
+      date: new Date(2022, 9, 29).toDateString(),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      {/* <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem> */}
     </div>
   );
 }
