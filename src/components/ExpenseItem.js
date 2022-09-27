753739627c3c829831b00461c8958484271a29d9
Abrar <amahi2001@gmail.com>
2022-09-27 13:44:51 -0400

    creating a component 'ExpenseItem'

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>September 28th 2022</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$294.3</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
