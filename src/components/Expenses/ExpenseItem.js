8c73181ed922c7efca0ca3a6a9e9a8a8ffc237f8
Abrar <amahi2001@gmail.com>
2022-09-29 12:33:58 -0400

    organized components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,6 @@
 import "./ExpenseItems.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
-
+import Card from "../UI/Card";
 //props are k, v pairs
 function ExpenseItem(props) {
   return (
