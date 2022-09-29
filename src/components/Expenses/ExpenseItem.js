be12ef0ace0aabae92b8510b2e7bf9953fa1de7e
Abrar Mahi <amahi2001@gmail.com>
2022-09-29 14:01:00 -0400

    displaying data synamically after events using the 'useState' Hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,23 @@
 import "./ExpenseItems.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import React, { useState } from "react";
 
 
 //props are k, v pairs
 const ExpenseItem = (props) => {
+
+  //this is a hook, can only be called inside of component functions
+  const [title, setTitle] = useState(props.title); 
+
   const onButtonClick = () => {
-    console.log('Clicked!')
+    setTitle('updated');
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={onButtonClick}>Change Title</button>
