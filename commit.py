feeb2892e6034be3b98449dcd72bec33c3573f47
Abrar Mahi <amahi2001@gmail.com>
2022-09-29 14:34:34 -0400

    **ignore** minor chnages to commit automation

--- commit.py
+++ commit.py
@@ -1,7 +1,9 @@
 import sys, os
 
 args = sys.argv
+if len(args) != 2:
+    raise ValueError(f'invalid commit message: {args[1:]}')
 msg = args[1]
 os.system('git add .')
-os.system(f' git commit -m {msg}')
+os.system(f'git commit -m "{msg}"')
 os.system(f'git push')
