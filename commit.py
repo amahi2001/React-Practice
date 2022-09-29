75c7ede6a462ef084dc7bba6b49c8fe8a40e773a
Abrar Mahi <amahi2001@gmail.com>
2022-09-29 14:47:01 -0400

    **ignore** editing commit automation

--- commit.py
+++ commit.py
@@ -2,9 +2,8 @@ import sys, os
 args = sys.argv
 if len(args) > 3:
     raise ValueError(f'invalid commit message: {args[1:]}')
-if len(args) == 3:
-    if args[2] != '-i':
-        raise ValueError(f'flag doesn\'t exist: {args[2]}')
+if len(args) == 3 and args[2] != '-i':
+    raise ValueError(f'flag doesn\'t exist: {args[2]}')
 msg = args[1] if "-i" not in args else f"**ignore** {args[1]}"
 os.system('git add .')
 os.system(f'git commit -m "{msg}"')
