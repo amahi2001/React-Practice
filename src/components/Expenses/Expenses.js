564c97eed3f3b0b80833460b069bc844d065da43
Abrar <amahi2001@gmail.com>
2022-10-03 16:54:25 -0400

    filtering array to display components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,19 +5,19 @@ import ExpensesFilter from "./ExpenseFilter";
 import { useState } from "react";
 
 const Expenses = (props) => {
-  const [yearInputState, setYearInputState] = useState("2022");
+  const [filteredYear, setFilteredYearState] = useState("2022");
   const onChangeYearHandler = (year) => {
-    setYearInputState(year);
+    setFilteredYearState(year);
   };
 
   return (
     <Card className="expenses">
       <ExpensesFilter
-        selectedYear={yearInputState}
+        selectedYear={filteredYear}
         onChangeYear={onChangeYearHandler}
       />
       {/* displaying components dynamically using map */}
-      {props.items.map((i) => (
+      {props.items.filter(i => i.date.getFullYear().toString().includes(filteredYear)).map((i) => (
         <ExpenseItem key = {i.id} title={i.title} amount={i.amount} date={i.date} />
       ))}
     </Card>
