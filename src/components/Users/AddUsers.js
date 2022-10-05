b2953be191386137db6668438460c5694d812ed1
Abrar <amahi2001@gmail.com>
2022-10-05 16:19:17 -0400

    added user info to states

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,20 +1,35 @@
 import Card from "../UI/Card";
-import styles from './AddUser.module.css'
+import styles from "./AddUser.module.css";
 import Button from "../UI/Button";
 
+import { useState } from "react";
+
 export default function AddUser(props) {
+  //username
+  const [EnteredUserName, setEnteredUserName] = useState("");
+  function usernameChangeHandler(event) {
+    setEnteredUserName(event.target.value);
+  }
+  //age
+  const [EnteredAge, setEnteredAge] = useState("");
+  function ageChangeHandler(event) {
+    setEnteredAge(event.target.value);
+  }
+
   function onSubmitForm(event) {
+    //submitting form to add user
     event.preventDefault();
-    props.onAddUser();
+    // props.onAddUser();
+    console.log(EnteredUserName, EnteredAge);
   }
 
   return (
     <Card className={styles.input}>
       <form onSubmit={onSubmitForm}>
         <label htmlFor="username">username</label>
-        <input id="username" type="text" />
+        <input onChange={usernameChangeHandler} id="username" type="text" />
         <label htmlFor="age">Age (years)</label>
-        <input id="age" type="number" />
+        <input onChange={ageChangeHandler} id="age" type="number" />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
